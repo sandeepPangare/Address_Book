6b1a58762c86c17726ca67e1f8af9f2c0015a91e
sandeepPangare <pangaresandeep96@gmail.com>
2022-11-17 22:59:02 +0530

    Added:Create a Contact in Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -5,5 +5,15 @@ public class AddressBook {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
+        Contact contacts = new Contact("Sandeep","Pangare","Nandurbar","Nandurbar","Maharashtra","pangaresandeep96@gmail.com","425424","7767029534");
+
+        System.out.println("First Name => "+contacts.getFirstName());
+        System.out.println("Last Name => "+contacts.getLastName());
+        System.out.println("Address => "+contacts.getAddress());
+        System.out.println("City => "+contacts.getCity());
+        System.out.println("State => "+contacts.getState());
+        System.out.println("Email => "+contacts.getEmail());
+        System.out.println("Phone Number => "+contacts.getPhoneNumber());
+
     }
 }
